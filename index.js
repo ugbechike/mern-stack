7b9b0c81372202fd6807f3eb407dccf2841b827d
ugbejohn <engr.ugbechike@gmail.com>
2019-11-17 01:38:32 +0400

    install body-parser and create first route

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import MockedUsers from './mocks/mockUsers'
 
 const app = express();
 const port = process.env.PORT;
+
+// todo abstract to a route folder
+app.get('/v1/users', (req, res) => {
+    res.send(MockedUsers);
+})
 
 app.listen(port, () => {
     console.log(`app is running on PORT: ${port}`)
---
+++ mocks/mockUsers.js
@@ -0,0 +1,35 @@
+const Users = [
+    {
+        id: '1234',
+        username: "John",
+        email: "john@mart.com",
+        email: "1234555"
+    },
+    {
+        id: '12345',
+        username: "Chike",
+        email: "john@mart.com",
+        email: "1234555"
+    },
+    {
+        id: '123456',
+        username: "Moi",
+        email: "john@mart.com",
+        email: "1234555"
+    },
+    {
+        id: '1234567',
+        username: "bobby",
+        email: "john@mart.com",
+        email: "1234555"
+    },
+    {
+        id: '12345678',
+        username: "mark",
+        email: "john@mart.com",
+        email: "1234555"
+    },
+
+]
+
+export default Users
