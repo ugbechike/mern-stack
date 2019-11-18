edf62b4a9314fcddfb0823fae5fbd72d6586dd0e
ugbejohn <engr.ugbechike@gmail.com>
2019-11-18 22:21:41 +0400

    read the user data from mongdb

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import logger from './middleware/logger';
 import withAuthentication from './middleware/withAuthentication'
 import MockedUsers from './mocks/mockUsers';
+import { UserModel } from './models/user'
+import { db } from './db/db';
 
 const app = express();
 // middleware for logging the request methods
@@ -10,13 +12,31 @@ app.use(logger);
 const port = process.env.PORT;
 
 // todo abstract to a route folder
-app.get('/v1/users', (req, res) => {
-    res.send(MockedUsers);
+app.get('/v1/users', async (req, res) => {
+    const users = await UserModel.find() || [];
+    res.send(users);
 })
 
-app.get('/v1/users/:id', (req, res) => {
-    res.send(MockedUsers[0]);
+app.get('/v1/users/:id', async (req, res) => {
+try {
+
+    const {id} = req.params
+    const userById = await UserModel.findById(id)
+    if (userById){
+        res.send(userById);
+
+    }else {
+        res.status(404).end
+    }
+} catch (err) {
+
+    console.log(err);
+    res.status(404).end();
+}
+
 })
+
+
 app.post('/v1/users', (req, res) => {
     const {userName, email, role, password} = req.body
     res.status(200).end;
